b52e826352b7ae71836a1cd56018c618c0aa0aae
Ian <iankosgey535@gmail.com>
2024-01-11 00:02:16 +0300

    Fetching on demand

--- src/index.js
+++ src/index.js
@@ -1,5 +1,16 @@
 const init = () => {
-  
+  const inputForm = document.querySelector('form');
+  inputForm.addEventListener('submit', e => {
+    e.preventDefault();
+    // const input = this.querySelector('input#searchById');
+    // console.log(e);
+
+    fetch(`http://localhost:3000/movies`)
+    .then(res => res.json())
+    .then( data => {
+        console.log(data);
+    })
+  })
 }
 
 document.addEventListener('DOMContentLoaded', init);
